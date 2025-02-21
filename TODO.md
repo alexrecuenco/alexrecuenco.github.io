75046abcedc462ec838da28d013e6bb9e87c03b2
alexrecuenco <26118630+alexrecuenco@users.noreply.github.com>
2025-02-21 02:58:10 +0100

    Add sitemap and mdx integration

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO list
+
+- [ ] Stop using google fonts, change fonts to something you like
+- [ ] Make it yours
+- [ ] Add linting and formatting
+- [ ] Add tests in Astro
+- [ ] Add GitHub actions for publishing and a CNAME file that generates
+- [ ] i18n
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'static',
+    site: import.meta.env.SITE_URL || 'https://portfolio.alexrecuenco.com',
+    integrations: [mdx(), sitemap()],
+    env: {
+        schema: {
+            PORT: envField.number({access: 'public',gt:0, lt:65536, default: 3000, context: 'server'}),
+        }
+    }
+});
